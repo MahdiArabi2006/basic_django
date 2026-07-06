f16fbb3e362de2c36be809df4b03cc7cb44e31f3
MahdiArabi2006 <mahdi.ar1384.ir@gmail.com>
2026-07-07 03:05:02 +0330

    m

--- backend/django-project/calender/settings.py
+++ backend/django-project/calender/settings.py
@@ -167,7 +167,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "mahdi.ar1384.ir@gmail.com"
-EMAIL_HOST_PASSWORD = "lgav cspw jbaa lqwo"
-
-
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
